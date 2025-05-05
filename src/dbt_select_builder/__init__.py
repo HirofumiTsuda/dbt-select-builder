53c05bfaaf67d6d870212a62d7ae47688a43c6bc
HirofumiTsuda <shimashimanishimashima@gmail.com>
2025-05-05 13:43:28 +0900

    initial commit

---
+++ src/dbt_select_builder/__init__.py
@@ -0,0 +1,1 @@
+from dbt_select_builder.operator import dbt_and, dbt_or
---
+++ src/dbt_select_builder/operator_test.py
@@ -0,0 +1,29 @@
+from dbt_select_builder import dbt_and, dbt_or
+import pytest
+
+
+@pytest.mark.parametrize(
+    "args, expected",
+    [
+        (
+            dbt_and("arg1", "arg2"),
+            "arg1,arg2"
+        ),
+        (
+            dbt_or(
+            "arg1", "arg2"
+            ),
+            "arg1 arg2"
+        ),
+        (
+            dbt_or(
+                dbt_and("arg1", "arg2"),
+                "arg3,arg4 arg5"
+            ),
+            "arg1,arg2 arg3,arg4 arg5"
+        ),
+    ]
+)
+def test_operator(args, expected: str) -> None:
+    """Test the operator functions."""
+    assert args.resolve().build() == expected
